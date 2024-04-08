d751c7b2458074dfe8b344886f84dbdd44a1a806
Jabez38 <jabezraja3111997@gmail.com>
2024-04-08 18:19:08 +0530

    jj

--- src/assets/assets/js/main.js
+++ src/assets/assets/js/main.js
@@ -15,28 +15,8 @@ function imgGallery() {
 imgGallery();
 
 
-/=============== SHOW MENU ===============/
-const navMenu = document.getElementById("nav-menu"),
-  navToggle = document.getElementById("nav-toggle"),
-  navClose = document.getElementById("nav-close");
-
-/===== Menu Show =====/
-/* Validate if constant exists */
-if (navToggle) {
-  navToggle.addEventListener("click", () => {
-    navMenu.classList.add("show-menu");
-  });
-}
-
-/===== Hide Show =====/
-/* Validate if constant exists */
-if (navClose) {
-  navClose.addEventListener("click", () => {
-    navMenu.classList.remove("show-menu");
-  });
-}
-
 /=============== IMAGE GALLERY ===============/
+
 function imgGallery() {
   const mainImg = document.querySelector(".details__img"),
     smallImg = document.querySelectorAll(".details__small-img");
@@ -48,7 +28,9 @@ function imgGallery() {
 }
 
 imgGallery();
+
 function categoryContainer() {
+
   /=============== SWIPER CATEGORIES ===============/
   let swiperCategories = new Swiper(".categories__container", {
     spaceBetween: 24,
@@ -87,6 +69,7 @@ function categoryContainer() {
     },
   });
 }
+
 function productContainer() {
 
   /=============== SWIPER PRODUCTS ===============/
@@ -120,183 +103,183 @@ function productContainer() {
   });
 }
 
+// /=============== PRODUCTS TABS ===============/
+// const tabs = document.querySelectorAll("[data-target]"),
+//   tabsContents = document.querySelectorAll("[content]");
 
-/=============== PRODUCTS TABS ===============/
-const tabs = document.querySelectorAll("[data-target]"),
-  tabsContents = document.querySelectorAll("[content]");
+// tabs.forEach((tab) => {
+//   tab.addEventListener("click", () => {
+//     const target = document.querySelector(tab.dataset.target);
 
-tabs.forEach((tab) => {
-  tab.addEventListener("click", () => {
-    const target = document.querySelector(tab.dataset.target);
-
-    tabsContents.forEach((tabsContent) => {
-      tabsContent.classList.remove("active-tab");
-    });
-
-    target.classList.add("active-tab");
-
-    tabs.forEach((tab) => {
-      tab.classList.remove("active-tab");
-    });
-
-    tab.classList.add("active-tab");
-  });
-});
-
-
-
-// document.addEventListener('DOMContentLoaded', () => {
-//   "use strict";
-
-//   /**
-//    * Preloader
-//    */
-//   const preloader = document.querySelector('#preloader');
-//   if (preloader) {
-//     window.addEventListener('load', () => {
-//       preloader.remove();
-//     });
-//   }
-
-//   /**
-//    * Sticky header on scroll
-//    */
-//   const selectHeader = document.querySelector('#header');
-//   if (selectHeader) {
-//     document.addEventListener('scroll', () => {
-//       window.scrollY > 100 ? selectHeader.classList.add('sticked') : selectHeader.classList.remove('sticked');
-//     });
-//   }
-
-//   /**
-//    * Scroll top button
-//    */
-//   const scrollTop = document.querySelector('.scroll-top');
-//   if (scrollTop) {
-//     const togglescrollTop = function () {
-//       window.scrollY > 100 ? scrollTop.classList.add('active') : scrollTop.classList.remove('active');
-//     }
-//     window.addEventListener('load', togglescrollTop);
-//     document.addEventListener('scroll', togglescrollTop);
-//     scrollTop.addEventListener('click', window.scrollTo({
-//       top: 0,
-//       behavior: 'smooth'
-//     }));
-//   }
-
-//   /**
-//    * Mobile nav toggle
-//    */
-//   const mobileNavShow = document.querySelector('.mobile-nav-show');
-//   const mobileNavHide = document.querySelector('.mobile-nav-hide');
-
-//   document.querySelectorAll('.mobile-nav-toggle').forEach(el => {
-//     el.addEventListener('click', function (event) {
-//       event.preventDefault();
-//       mobileNavToogle();
-//     })
-//   });
-
-//   function mobileNavToogle() {
-//     document.querySelector('body').classList.toggle('mobile-nav-active');
-//     mobileNavShow.classList.toggle('d-none');
-//     mobileNavHide.classList.toggle('d-none');
-//   }
-
-//   /**
-//    * Hide mobile nav on same-page/hash links
-//    */
-//   document.querySelectorAll('#navbar a').forEach(navbarlink => {
-
-//     if (!navbarlink.hash) return;
-
-//     let section = document.querySelector(navbarlink.hash);
-//     if (!section) return;
-
-//     navbarlink.addEventListener('click', () => {
-//       if (document.querySelector('.mobile-nav-active')) {
-//         mobileNavToogle();
-//       }
+//     tabsContents.forEach((tabsContent) => {
+//       tabsContent.classList.remove("active-tab");
 //     });
 
-//   });
+//     target.classList.add("active-tab");
 
-//   /**
-//    * Toggle mobile nav dropdowns
-//    */
-//   const navDropdowns = document.querySelectorAll('.navbar .dropdown > a');
-
-//   navDropdowns.forEach(el => {
-//     el.addEventListener('click', function (event) {
-//       if (document.querySelector('.mobile-nav-active')) {
-//         event.preventDefault();
-//         this.classList.toggle('active');
-//         this.nextElementSibling.classList.toggle('dropdown-active');
-
-//         let dropDownIndicator = this.querySelector('.dropdown-indicator');
-//         dropDownIndicator.classList.toggle('bi-chevron-up');
-//         dropDownIndicator.classList.toggle('bi-chevron-down');
-//       }
-//     })
-//   });
-
-//   /**
-//    * Initiate pURE cOUNTER
-//    */
-//   new PureCounter();
-
-//   /**
-//    * Initiate glightbox
-//    */
-//   const glightbox = GLightbox({
-//     selector: '.glightbox'
-//   });
-
-//   /**
-//    * Init swiper slider with 1 slide at once in desktop view
-//    */
-//   new Swiper('.slides-1', {
-//     speed: 600,
-//     loop: true,
-//     autoplay: {
-//       delay: 5000,
-//       disableOnInteraction: false
-//     },
-//     slidesPerView: 'auto',
-//     pagination: {
-//       el: '.swiper-pagination',
-//       type: 'bullets',
-//       clickable: true
-//     },
-//     navigation: {
-//       nextEl: '.swiper-button-next',
-//       prevEl: '.swiper-button-prev',
-//     },
-
-
-//   });
-
-
-
-
-
-
-/**
- * Animation on scroll function and init
- */
-//   function aos_init() {
-//     AOS.init({
-//       duration: 1000,
-//       easing: 'ease-in-out',
-//       once: true,
-//       mirror: false
+//     tabs.forEach((tab) => {
+//       tab.classList.remove("active-tab");
 //     });
-//   }
-//   window.addEventListener('load', () => {
-//     aos_init();
-//   });
 
+//     tab.classList.add("active-tab");
+//   });
 // });
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  "use strict";
+
+  /**
+   * Preloader
+   */
+  const preloader = document.querySelector('#preloader');
+  if (preloader) {
+    window.addEventListener('load', () => {
+      preloader.remove();
+    });
+  }
+
+  /**
+   * Sticky header on scroll
+   */
+  const selectHeader = document.querySelector('#header');
+  if (selectHeader) {
+    document.addEventListener('scroll', () => {
+      window.scrollY > 100 ? selectHeader.classList.add('sticked') : selectHeader.classList.remove('sticked');
+    });
+  }
+
+  /**
+   * Scroll top button
+   */
+  const scrollTop = document.querySelector('.scroll-top');
+  if (scrollTop) {
+    const togglescrollTop = function () {
+      window.scrollY > 100 ? scrollTop.classList.add('active') : scrollTop.classList.remove('active');
+    }
+    window.addEventListener('load', togglescrollTop);
+    document.addEventListener('scroll', togglescrollTop);
+    scrollTop.addEventListener('click', window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    }));
+  }
+
+  /**
+   * Mobile nav toggle
+   */
+  const mobileNavShow = document.querySelector('.mobile-nav-show');
+  const mobileNavHide = document.querySelector('.mobile-nav-hide');
+
+  document.querySelectorAll('.mobile-nav-toggle').forEach(el => {
+    el.addEventListener('click', function (event) {
+      event.preventDefault();
+      mobileNavToogle();
+    })
+  });
+
+  function mobileNavToogle() {
+    document.querySelector('body').classList.toggle('mobile-nav-active');
+    mobileNavShow.classList.toggle('d-none');
+    mobileNavHide.classList.toggle('d-none');
+  }
+
+  /**
+   * Hide mobile nav on same-page/hash links
+   */
+  document.querySelectorAll('#navbar a').forEach(navbarlink => {
+
+    if (!navbarlink.hash) return;
+
+    let section = document.querySelector(navbarlink.hash);
+    if (!section) return;
+
+    navbarlink.addEventListener('click', () => {
+      if (document.querySelector('.mobile-nav-active')) {
+        mobileNavToogle();
+      }
+    });
+
+  });
+
+  /**
+   * Toggle mobile nav dropdowns
+   */
+  const navDropdowns = document.querySelectorAll('.navbar .dropdown > a');
+
+  navDropdowns.forEach(el => {
+    el.addEventListener('click', function (event) {
+      if (document.querySelector('.mobile-nav-active')) {
+        event.preventDefault();
+        this.classList.toggle('active');
+        this.nextElementSibling.classList.toggle('dropdown-active');
+
+        let dropDownIndicator = this.querySelector('.dropdown-indicator');
+        dropDownIndicator.classList.toggle('bi-chevron-up');
+        dropDownIndicator.classList.toggle('bi-chevron-down');
+      }
+    })
+  });
+
+  /**
+   * Initiate pURE cOUNTER
+   */
+
+  new PureCounter();
+
+  /**
+   * Initiate glightbox
+   */
+  const glightbox = GLightbox({
+    selector: '.glightbox'
+  });
+
+  /**
+   * Init swiper slider with 1 slide at once in desktop view
+   */
+  new Swiper('.slides-1', {
+    speed: 600,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    slidesPerView: 'auto',
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+
+  });
+
+
+
+
+
+
+  /**
+   * Animation on scroll function and init
+   */
+  // function aos_init() {
+  //   AOS.init({
+  //     duration: 1000,
+  //     easing: 'ease-in-out',
+  //     once: true,
+  //     mirror: false
+  //   });
+  // }
+  // window.addEventListener('load', () => {
+  //   aos_init();
+  // });
+
+});
 
 
 
@@ -311,9 +294,13 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   });
 
-  closeBtn.addEventListener('click', function () {
-    modal.style.display = 'none';
-  });
+  try {
+    closeBtn.addEventListener('click', function () {
+      modal.style.display = 'none';
+    });
+  } catch (e) {
+    console.log(e);
+  }
 
   window.addEventListener('click', function (event) {
     if (event.target === modal) {
@@ -321,6 +308,23 @@ document.addEventListener('DOMContentLoaded', function () {
     }
   });
 });
+
+
+
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+  if (document.body.scrollTop > 4500 || document.documentElement.scrollTop > 4500) {
+    document.querySelector('.scroll-top-btn').classList.add('show');
+  } else {
+    document.querySelector('.scroll-top-btn').classList.remove('show');
+  }
+}
+
+function scrollToTop() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
 
 
 // function productContainer() {
